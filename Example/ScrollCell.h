95764ea9ade700a32c8caaee92ad111c0fec6070
JKTableView <jitu94186@gmail.com>
2016-03-19 14:09:26 +0530

    hello
    something new

---
+++ Example/ScrollCell.h
@@ -0,0 +1,13 @@
+//
+//  ScrollCell.h
+//  ScrollViewTable
+//
+//  Created by Zakoopi on 18/03/16.
+//  Copyright © 2016 JKTableView. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ScrollCell : UIScrollView
+
+@end
---
+++ Example/ScrollCell.m
@@ -0,0 +1,21 @@
+//
+//  ScrollCell.m
+//  ScrollViewTable
+//
+//  Created by Zakoopi on 18/03/16.
+//  Copyright © 2016 JKTableView. All rights reserved.
+//
+
+#import "ScrollCell.h"
+
+@implementation ScrollCell
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
